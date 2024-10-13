2cd3ce70ba9009cb195d72078497a0612953c6e6
Denis Korablev <info@codeweber.com>
2024-10-13 15:10:46 +0300

    features 101+

--- codeweber/templates/flexible-content/features/features_101/features_101.php
+++ codeweber/templates/flexible-content/features/features_101/features_101.php
@@ -30,11 +30,11 @@ $block = new CW_Settings(
     'features_pattern' => '<div class="col-lg-4 %1$s"><div class="d-flex flex-col card card-body h-100 lift"><div class="d-flex align-items-center mb-3">%2$s %3$s</div><div><p class="mb-2">%4$s</p>%5$s</div></div></div><!--/column -->',
     'features_style_icon' => 'me-4',
 
-    'title' => 'Who Are We?',
-    'patternTitle' => '<h2 class="display-4 mb-3 %2$s">%1$s</h2>',
+    //'title' => 'Who Are We?',
+    //'patternTitle' => '<h2 class="display-4 mb-3 %2$s">%1$s</h2>',
 
-    'subtitle' => 'We are a digital and branding company that believes in the power of creative strategy and along with great design.',
-    'patternSubtitle' => '<p class="lead fs-lg %2$s">%1$s</p>',
+    //'subtitle' => 'We are a digital and branding company that believes in the power of creative strategy and along with great design.',
+    //'patternSubtitle' => '<p class="lead fs-lg %2$s">%1$s</p>',
 
   )
 );
@@ -43,19 +43,7 @@ $block = new CW_Settings(
 
 <section id="<?php echo esc_html($args['block_id']); ?>" class="<?php echo $block->section_class; ?> <?php echo esc_html($args['block_class']); ?>" <?php echo $block->background_data; ?>>
   <div class="container py-14 py-md-16">
-    <div class="row">
-      <div class="col-lg-8 col-xl-7 col-xxl-6 mb-5">
-        <?php echo $block->subtitle_first; ?>
-        <!--/subtitle -->
-        <?php echo $block->title; ?>
-        <!--/title -->
-        <?php echo $block->subtitle_second; ?>
-        <!--/subtitle -->
-      </div>
-      <!-- /column -->
-    </div>
-    <!-- /row -->
-    <div class="row gx-lg-6 gx-xl-6 gy-6">
+    <div class="row gx-md-5 gy-5 mt-n18 mt-md-n21">
       <?php echo $block->features; ?>
     </div>
     <!--/.row -->
