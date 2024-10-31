6e31b0841f19f055c8332e5dd56228e52b2682f0
Denis Korablev <info@codeweber.com>
2024-10-31 13:50:43 +0300

    project2

--- functions/bootstrap/bootstrap_post-nav.php
+++ functions/bootstrap/bootstrap_post-nav.php
@@ -10,11 +10,11 @@ function codeweber_posts_nav()
       <!--/column -->
       <div class="col-md-8 align-self-center text-center text-md-start navigation">
          <?php if (!empty($prev_post)) : ?>
-            <a href="<?php echo get_permalink($prev_post); ?>" class="btn btn-soft-ash <?php echo GetThemeButton(); ?> btn-icon btn-icon-start mb-0 me-1"><i class="uil uil-arrow-left"></i> <?php _e('Prev', 'codeweber') ?></a>
+            <a href="<?php echo get_permalink($prev_post); ?>" class="btn btn-soft-ash btn-md btn-icon btn-icon-start mb-0 me-1"><i class="uil uil-arrow-left"></i> <?php _e('Prev', 'codeweber') ?></a>
          <?php endif; ?>
 
          <?php if (!empty($next_post)) : ?>
-            <a href="<?php echo get_permalink($next_post); ?>" class="btn btn-soft-ash <?php echo GetThemeButton(); ?> btn-icon btn-icon-end mb-0"><?php _e('Next', 'codeweber') ?> <i class="uil uil-arrow-right"></i></a>
+            <a href="<?php echo get_permalink($next_post); ?>" class="btn btn-soft-ash btn-md btn-icon btn-icon-end mb-0"><?php _e('Next', 'codeweber') ?> <i class="uil uil-arrow-right"></i></a>
          <?php endif; ?>
       </div>
       <!--/column -->
