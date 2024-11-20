1c09ce8bc287c6b7686988dff6e40b21f9ae4087
Denis Korablev <info@codeweber.com>
2024-11-20 15:17:45 +0300

    woo 4

--- templates/flexible-content/woocommerce/woocommerce_4/woocommerce_4.php
+++ templates/flexible-content/woocommerce/woocommerce_4/woocommerce_4.php
@@ -27,11 +27,11 @@ $block = new CW_Settings(
             <?php echo $block->subtitle_second; ?>
             <!--/subtitle -->
          </div>
-         <div class="woo-3 grid grid-view projects-masonry shop">
-            <div class="row  isotope">
+         <div class="grid grid-view projects-masonry shop">
+            <div class="row g-5 isotope">
                <?php $product = get_sub_field('product'); ?>
                <?php if ($product) {
-                   foreach ($product as $post_ids) {
+                  foreach ($product as $post_ids) {
                      $main_image_id = get_post_thumbnail_id($post_ids);
                      $gallery_images = get_post_meta($post_ids, '_product_image_gallery', true);
                      $gallery_images_ids = $gallery_images ? explode(',', $gallery_images) : [];
@@ -42,31 +42,53 @@ $block = new CW_Settings(
                      if (has_post_thumbnail($post_ids)) {
                         $image_html = get_the_post_thumbnail($post_ids, 'archive_4', ['class' => 'img-fluid']);
                         $image_link = get_the_post_thumbnail_url($post_ids, 'sandbox_hero_18');
-
                      } else {
                         $image_html = '<img src="'
-                        . get_template_directory_uri().'/dist/img/placeholder_600x600.jpeg" class="img-fluid" alt="Placeholder Image">';
+                           . get_template_directory_uri() . '/dist/img/placeholder_600x600.jpeg" class="img-fluid" alt="Placeholder Image">';
                         $image_link = get_template_directory_uri() . '/dist/img/placeholder_600x600.jpeg';
                      }
-                     ?>
-                     <div class="project item col-12 col-md-4 col-lg-4 col-xl-4">
-                        <div class="position-relative lift">
-                           <a data-glightbox href="<?php echo $image_link; ?>" data-gallery="<?php echo $post_ids; ?>" class="position-relative
+               ?>
+                     <div class="project item col-12 col-md-6 col-lg-6 col-xl-6">
+                        <div class="position-relative">
+                           <a data-glightbox href="<?php echo $image_link; ?>" data-gallery="<?php echo $post_ids; ?><?php echo esc_html($args['block_id']); ?>" class="position-relative card lift
     ">
-                              <figure class="rounded mb-6">
-                                 <?php echo $image_html;
-                                 ?>
-                              </figure>
-                              <h2 class="rounded overflow-hidden title-wrap-cw position-absolute ps-3 pe-10 w-100 pb-3 pt-10 mb-0 bottom-0 start-0 pb-0 mb-0 post-title display-6 fs-18 text-white  woocommerce-loop-product__title">
-                                 <?php echo get_the_title($post_ids); ?>
-                              </h2>
+                              <div class="row align-items-center">
+                                 <div class="col-lg-6">
+                                    <figure class="rounded">
+                                       <?php echo $image_html;
+                                       ?>
+                                       <?php if ($total_images_count > 1) { ?>
+                                          <div class="count_image text-white position-absolute bottom-0 end-0 p-3">
+                                             <span class="icon_count_wrap"><i class="uil uil-images"></i></span>
+                                             <span class="count_wrap"><?php echo $total_images_count; ?></span>
+                                          </div>
+                                       <?php } ?>
+                                    </figure>
+                                 </div>
 
-                              <?php if($total_images_count > 1){ ?>
-                              <div class="count_image text-white position-absolute bottom-0 end-0 p-3">
-                                 <span class="icon_count_wrap"><i class="uil uil-images"></i></span>
-                                 <span class="count_wrap"><?php echo $total_images_count; ?></span>
+                                 <div class="col-lg-6">
+                                    <div class="p-4">
+                                       <h2 class="display-6 fs-18  woocommerce-loop-product__title">
+                                          <?php echo get_the_title($post_ids); ?>
+                                       </h2>
+
+                                       <?php
+                                       $product = wc_get_product($post_ids);
+                                       if ($product) {
+                                          $description = $product->get_description();
+
+                                          if (!empty($description)) {
+                                             echo '<div class="product-description">' . esc_html($description) . '</div>';
+                                          }
+                                       }
+                                       ?>
+                                    </div>
+
+
+                                 </div>
+
+
                               </div>
-                              <?php } ?>
                            </a>
                         </div>
                      </div>
@@ -74,28 +96,26 @@ $block = new CW_Settings(
                      $gallery_images = get_post_meta($post_ids, '_product_image_gallery', true);
                      $gallery_images_ids = $gallery_images ? explode(',', $gallery_images) : []; ?>
                      <div class="d-none"><?php if (!empty($gallery_images_ids)) {
-                            $product_id = $post_ids;
-                            $count = 0; // Инициализируем счётчик
-                            foreach ($gallery_images_ids as $image_id) {
+                                             $product_id = $post_ids;
+                                             $count = 0; // Инициализируем счётчик
+                                             foreach ($gallery_images_ids as $image_id) {
                                                 $count++;
-                                                if ($count === 1) {
-                                                   continue;
-                                                }
-                                  $gallery_image = wp_get_attachment_image_src($image_id, 'sandbox_hero_18');
-                                   $gallery_image_url = $gallery_image ? $gallery_image[0] : ''; ?>
+                                                
+                                                $gallery_image = wp_get_attachment_image_src($image_id, 'sandbox_hero_18');
+                                                $gallery_image_url = $gallery_image ? $gallery_image[0] : ''; ?>
                               <figure class="rounded mb-6">
                                  <img
                                     data-glightbox
-                                    data-gallery="<?php echo $post_ids; ?>"
+                                    data-gallery="<?php echo $post_ids; ?><?php echo esc_html($args['block_id']); ?>"
                                     src="<?php echo esc_url($gallery_image_url); ?>"
                                     alt="Gallery Image"
                                     class="img-fluid">
                               </figure>
-                           <?php };
-                         }; ?>
+                        <?php };
+                                          }; ?>
                      </div>
-                  <?php };
-                }; ?>
+               <?php };
+               }; ?>
             </div>
          </div>
          <?php the_sub_field('shortcode'); ?>
